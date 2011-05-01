b65d562ad8427fcbfe5fe1dd37ed8c00e9a06cea
minolo3ds@gmail.com <minolo3ds@gmail.com@13b265a8-7f8f-edb1-3fef-2cd1eb4cdfcb>
2011-05-01 22:03:30 +0000

    Layer!

--- trunk/neural_network/src/main/Test.java
+++ trunk/neural_network/src/main/Test.java
@@ -1,24 +1,41 @@
 package main;
 
+import neural_network.Layer;
 import neural_network.Perceptron;
 import neural_network.Value;
 
 public class Test {
+	
+	public static enum TestToRun {PERCEPTRON, LAYER};
+	public static final TestToRun test = TestToRun.PERCEPTRON;
 
 	public static void main(String[] args) {
-
-		Perceptron p = new Perceptron(4);
+		
+		switch(test) {
+			case PERCEPTRON:
+				perceptronTest();
+				break;
+			case LAYER:
+				layerTest();
+				break;
+		}
+		
+		System.out.println("Fin.");
+	}
+	
+	private static void perceptronTest() {
+		Perceptron perceptron = new Perceptron(3);
 		
 		// Training set
 		Value[][] training_set_inputs = {
-			{new Value(-1), new Value(0), new Value(0), new Value(0)},
-			{new Value(-1), new Value(1), new Value(0), new Value(0)},
-			{new Value(-1), new Value(0), new Value(1), new Value(0)},
-			{new Value(-1), new Value(1), new Value(1), new Value(0)},
-			{new Value(-1), new Value(0), new Value(0), new Value(1)},
-			{new Value(-1), new Value(1), new Value(0), new Value(1)},
-			{new Value(-1), new Value(0), new Value(1), new Value(1)},
-			{new Value(-1), new Value(1), new Value(1), new Value(1)}
+			{new Value(0), new Value(0), new Value(0)},
+			{new Value(1), new Value(0), new Value(0)},
+			{new Value(0), new Value(1), new Value(0)},
+			{new Value(1), new Value(1), new Value(0)},
+			{new Value(0), new Value(0), new Value(1)},
+			{new Value(1), new Value(0), new Value(1)},
+			{new Value(0), new Value(1), new Value(1)},
+			{new Value(1), new Value(1), new Value(1)}
 		};
 		
 		Value[] training_set_outputs = {
@@ -41,7 +58,7 @@ public class Test {
 			totalError = 0;
 			
 			for(int i = 0; i < training_set_inputs.length; i++)
-				totalError += p.deltaRule(training_set_inputs[i], training_set_outputs[i], learningRate);
+				totalError += perceptron.deltaRule(training_set_inputs[i], training_set_outputs[i], learningRate);
 			
 			iterations++;
 		} while(Math.abs(totalError) > 0.1);
@@ -51,11 +68,20 @@ public class Test {
 		
 		// Test
 		for(int i = 0; i < training_set_inputs.length; i++) {
-			p.setInputs(training_set_inputs[i]);
-			p.computeOutput();
-			System.out.println(p.getOutput().value > 0.5 ? 1 : 0);
+			perceptron.setInputs(training_set_inputs[i]);
+			perceptron.computeOutput();
+			System.out.println(perceptron.getOutput().value > 0.5 ? 1 : 0);
 		}
-		
 	}
-
+	
+	private static void layerTest() {
+		Layer layer1 = new Layer(4, 10);
+		Layer layer2 = new Layer(10, 7);
+		
+		layer1.connectTo(layer2);
+		
+		layer1.computeOutput();
+		layer2.computeOutput();
+	}
+	
 }
--- trunk/neural_network/src/neural_network/Perceptron.java
+++ trunk/neural_network/src/neural_network/Perceptron.java
@@ -6,27 +6,27 @@ public class Perceptron {
 
 	private Value[] inputs;
 	private Value[] weights;
+	private Value threshold;
 	
 	private Value output;
 	
 	public Perceptron(int numInputs) {
+		if(numInputs < 1)
+			throw new IllegalArgumentException("Perceptrons must have at least one input!");
+		
 		inputs = new Value[numInputs];
 		weights = new Value[numInputs];
 		
-		for(int i = 0; i < numInputs; i++)
+		for(int i = 0; i < numInputs; i++) {
+			inputs[i] = new Value(0);
 			weights[i] = new Value(0);
+		}
 		
+		threshold = new Value(0);
 		output = new Value(0);
 	}
 	
-	public void setInput(int index, Value value) {
-		inputs[index] = value;
-	}
-	
 	public void setInputs(Value[] inputs) {
-		if(inputs.length != this.inputs.length)
-			throw new IllegalArgumentException("Input array does not have the same length as the inputs array!");
-		
 		this.inputs = inputs;
 	}
 	
@@ -44,18 +44,19 @@ public class Perceptron {
 	}
 	
 	public void computeOutput() {
-		output.value = activationFunction(Util.dotProduct(inputs, weights));
+		output.value = activationFunction(Util.dotProduct(inputs, weights) - threshold.value);
 	}
 	
 	public float deltaRule(Value[] targetInput, Value targetOutput, float learningRate) {
 		if(targetInput.length != weights.length)
 			throw new IllegalArgumentException("Input array does not have the same length as the weights array!");
 		
-		float in = Util.dotProduct(targetInput, weights);
+		float in = Util.dotProduct(targetInput, weights) - threshold.value;
 		float difference = (targetOutput.value - activationFunction(in));
 		
 		float factor = learningRate * difference * derivativeOfActivationFunction(in);
-		 
+		
+		threshold.value -= factor;
 		for(int i = 0; i < weights.length; i++)
 			weights[i].value += (float)(factor * targetInput[i].value);
 		

29739cb3042434e63ac530f23cad2e085f43bf29
jacano35@gmail.com <jacano35@gmail.com@13b265a8-7f8f-edb1-3fef-2cd1eb4cdfcb>
2011-06-08 14:10:54 +0000

    a few method to allow easy neural network training using images

--- ia2_nngr/src/main/Main.java
+++ ia2_nngr/src/main/Main.java
@@ -8,58 +8,46 @@ import train.TrainingImage;
 import training.ITrainingInstance;
 import training.TrainingSet;
 import neural_network.NeuralNetwork;
+import neural_network.NeuralNetwork.LearningMethod;
 
 public class Main {
 
 	public static void main(String[] args) 
 	{
 		int numInputs = 30*30;
-		int numOutputs = 1;
-		int numHiddenNeurons = 10;
+		int numOutputs = 2;
+		int numHiddenNeurons = 100;
 		
 		NeuralNetwork nn = new NeuralNetwork(new int[]{numInputs, numHiddenNeurons, numOutputs});
 		
 		TrainingSet ts = new TrainingSet(numInputs, numOutputs);
 
-		String dir = "./train/hand/right/fingers/0/";
+		String dir1 = "./train/hand/right/fingers/0/";
 		
-		File actual = new File(dir);
-        for(File f : actual.listFiles())
-        {
-        	String filename = dir + f.getName();
-        	if(filename.endsWith(".png"))
-        	{
-        		TrainingImage ti = new TrainingImage(filename);
-        		
-        		double[] inputs = ti.getTrainingInputs();
-        		double[] outputs = new double[numOutputs];
-        		outputs[0] = 1;
-        		
-        		ITrainingInstance newInstance = new SimpleTrainingInstance(inputs, outputs);
-        		ts.addInstance(newInstance);
-        	}
-        }
+		double outputs1[] = new double[numOutputs];
+		outputs1[0] = 1;
+		outputs1[1] = 0;
+		trainPhotoDir(outputs1, ts, dir1);
+		
+		String dir2 = "./train/hand/right/fingers/5/";
+		
+		double outputs2[] = new double[numOutputs];
+		outputs2[0] = 0;
+		outputs2[1] = 1;
+		trainPhotoDir(outputs2, ts, dir2);
 		
 
 		// Train		
 		double learningRate = 0.1;
-		double momentum = 0.9;
+		double momentum = 0.98;
+		
+		int numIterations = 100;
+		int errorChecks = 50;
+		LearningMethod lm = NeuralNetwork.LearningMethod.INCREMENTAL;
 		
 		
-		double error = 0;
-		int iterations = 0;
-		for(iterations = 0; iterations < 1000; iterations++)
-		{
-			nn.train(NeuralNetwork.LearningMethod.INCREMENTAL, ts, 1000, learningRate, momentum);
-			
-			error = nn.rootMeanSquaredError(ts);
-			
-			System.out.println(error);
-			
-			if(Math.abs(error) < 0.001) break;
-		}
+		trainNeuralNetwork(nn, ts, learningRate, momentum, numIterations, errorChecks, lm, true);
 		
-		System.out.println(iterations + " " + error);
 		
 		// Test
 		for(ITrainingInstance instance : ts)
@@ -70,6 +58,44 @@ public class Main {
 		
 		
 		System.out.println("END");
+	}
+
+	private static void trainNeuralNetwork(NeuralNetwork nn, TrainingSet ts, double learningRate, double momentum, int numIterations, int errorChecks, LearningMethod lm, boolean verbose)
+	{
+		int iterations;
+		double error = 0;
+		
+		int iter = numIterations/errorChecks;
+		for(iterations = 0 ; iterations < errorChecks; iterations++)
+		{
+			nn.train(lm, ts, iter, learningRate, momentum);
+			
+			error = nn.rootMeanSquaredError(ts);
+			
+			if(verbose) System.out.println(error);
+			
+			if(Math.abs(error) < 0.001) break;
+		}
+		
+		if(verbose) System.out.println(iterations*iter + " " + error);
+	}
+	
+	private static void trainPhotoDir(double[] outputs, TrainingSet ts, String dir) 
+	{
+		File actual = new File(dir);
+        for(File f : actual.listFiles())
+        {
+        	String filename = dir + f.getName();
+        	if(filename.endsWith(".png"))
+        	{
+        		TrainingImage ti = new TrainingImage(filename);
+        		
+        		double[] inputs = ti.getTrainingInputs();
+        		
+        		ITrainingInstance newInstance = new SimpleTrainingInstance(inputs, outputs);
+        		ts.addInstance(newInstance);
+        	}
+        }
 	}
 
 }

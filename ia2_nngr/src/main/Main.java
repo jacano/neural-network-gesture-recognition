99eade3fb3a17c90718e4e2f786bedd02d094c9c
jacano35@gmail.com <jacano35@gmail.com@13b265a8-7f8f-edb1-3fef-2cd1eb4cdfcb>
2011-06-08 15:12:49 +0000

    ImageTrainingInstance added

--- ia2_nngr/src/main/Main.java
+++ ia2_nngr/src/main/Main.java
@@ -4,11 +4,12 @@ import java.io.File;
 import java.util.Arrays;
 
 
-import train.TrainingImage;
+import train.ImageTrainingInstance;
 import training.ITrainingInstance;
 import training.TrainingSet;
 import neural_network.NeuralNetwork;
 import neural_network.NeuralNetwork.LearningMethod;
+import neural_network.SimpleValue;
 
 public class Main {
 
@@ -24,16 +25,16 @@ public class Main {
 
 		String dir1 = "./train/hand/right/fingers/0/";
 		
-		double outputs1[] = new double[numOutputs];
-		outputs1[0] = 1;
-		outputs1[1] = 0;
+		SimpleValue outputs1[] = new SimpleValue[numOutputs];
+		outputs1[0] = new SimpleValue(1.0);
+		outputs1[1] = new SimpleValue(0.0);
 		trainImageDir(outputs1, ts, dir1);
 		
 		String dir2 = "./train/hand/right/fingers/5/";
 		
-		double outputs2[] = new double[numOutputs];
-		outputs2[0] = 0;
-		outputs2[1] = 1;
+		SimpleValue outputs2[] = new SimpleValue[numOutputs];
+		outputs2[0] = new SimpleValue(0.0);
+		outputs2[1] = new SimpleValue(1.0);
 		trainImageDir(outputs2, ts, dir2);
 		
 
@@ -43,7 +44,7 @@ public class Main {
 		
 		int numIterations = 100;
 		int errorChecks = 50;
-		LearningMethod lm = NeuralNetwork.LearningMethod.INCREMENTAL;
+		LearningMethod lm = NeuralNetwork.LearningMethod.BATCH;
 		
 		
 		trainNeuralNetwork(nn, ts, learningRate, momentum, numIterations, errorChecks, lm, true);
@@ -80,7 +81,7 @@ public class Main {
 		if(verbose) System.out.println(iterations*iter + " " + error);
 	}
 	
-	private static void trainImageDir(double[] outputs, TrainingSet ts, String dir) 
+	private static void trainImageDir(SimpleValue[] outputs, TrainingSet ts, String dir) 
 	{
 		File actual = new File(dir);
         for(File f : actual.listFiles())
@@ -88,12 +89,8 @@ public class Main {
         	String filename = dir + f.getName();
         	if(filename.endsWith(".png"))
         	{
-        		TrainingImage ti = new TrainingImage(filename);
-        		
-        		double[] inputs = ti.getTrainingInputs();
-        		
-        		ITrainingInstance newInstance = new SimpleTrainingInstance(inputs, outputs);
-        		ts.addInstance(newInstance);
+        		ImageTrainingInstance iti = new ImageTrainingInstance(filename, outputs);
+        		ts.addInstance(iti);
         	}
         }
 	}

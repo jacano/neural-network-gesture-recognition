4c504ea4126ac0e40b6450b2dcf39517cddeddb1
jacanosalado@gmail.com <jacanosalado@gmail.com@13b265a8-7f8f-edb1-3fef-2cd1eb4cdfcb>
2011-06-10 17:35:39 +0000

    pokemon

---
+++ trunk/ia2_nngr/src/train/PokemonTrain.java
@@ -0,0 +1,10 @@
+package train;
+
+import java.util.List;
+
+public class PokemonTrain
+{
+	public String networkName;
+	public List<String> dirs;
+	public TrainingParameters tp;
+}
---
+++ trunk/ia2_nngr/src/train/TrainingParameters.java
@@ -0,0 +1,15 @@
+package train;
+
+import neural_network.NeuralNetwork;
+import neural_network.NeuralNetwork.LearningMethod;
+
+public class TrainingParameters {
+
+	public double learningRate = 0.01;
+	public double momentum = 0.2;
+	public int numIterations = 10000;
+	public int errorChecks = 50;
+	public double maxError = 0.001;
+	public LearningMethod lm = NeuralNetwork.LearningMethod.BATCH;
+	
+}

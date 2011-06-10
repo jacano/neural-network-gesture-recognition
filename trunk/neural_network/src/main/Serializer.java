d6acdad476a677f8d21296988c8d90e972fea95e
jacanosalado@gmail.com <jacanosalado@gmail.com@13b265a8-7f8f-edb1-3fef-2cd1eb4cdfcb>
2011-06-10 02:12:25 +0000

    several serialization size improvements!!

--- trunk/neural_network/src/main/Serializer.java
+++ trunk/neural_network/src/main/Serializer.java
@@ -5,14 +5,20 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import neural_network.NeuralNetwork;
+import neural_network.Neuron;
+import neural_network.SimpleValue;
+
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
 
 
 public class Serializer {
 
+
 	public static void Serialize(String filename, Object o)
 	{
-		XStream xstream = new XStream();
+		XStream xstream = getXStream();
 		
 		FileOutputStream os = null;
 		try {
@@ -34,7 +40,7 @@ public class Serializer {
 	
 	public static Object DeSerialize(String filename)
 	{
-		XStream xstream = new XStream();
+		XStream xstream = getXStream();
 		
 		FileInputStream is = null;
 		try {
@@ -55,4 +61,31 @@ public class Serializer {
 		
 		return o;
 	}
+	
+	
+	private static XStream getXStream()
+	{
+		XStream xstream = new XStream(new JettisonMappedXmlDriver());
+		xstream.setMode(XStream.NO_REFERENCES);
+		
+		xstream.alias("nn", NeuralNetwork.class);
+		xstream.aliasField("ii", NeuralNetwork.class, "inputs");
+		xstream.aliasField("oo", NeuralNetwork.class, "output");
+		xstream.aliasField("nnn", NeuralNetwork.class, "neurons");
+		
+		xstream.alias("q", SimpleValue.class);
+		xstream.aliasField("", SimpleValue.class, "value");
+		
+		xstream.alias("n", Neuron.class);
+		xstream.aliasField("e", Neuron.class, "deltaWeights");
+		xstream.aliasField("w", Neuron.class, "weights");
+		xstream.aliasField("i", Neuron.class, "inputs");
+		xstream.aliasField("f", Neuron.class, "delta");
+		xstream.aliasField("o", Neuron.class, "output");
+		
+		xstream.alias("d", Double.TYPE);
+		
+		return xstream;
+	}
+	
 }

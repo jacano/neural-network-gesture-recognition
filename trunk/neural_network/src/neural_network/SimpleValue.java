e18d3d1a129032560758f0bc05bed7919aae5262
minolo3ds@gmail.com <minolo3ds@gmail.com@13b265a8-7f8f-edb1-3fef-2cd1eb4cdfcb>
2011-06-10 08:42:11 +0000

    Serialization 101

--- trunk/neural_network/src/neural_network/SimpleValue.java
+++ trunk/neural_network/src/neural_network/SimpleValue.java
@@ -1,7 +1,11 @@
 package neural_network;
 
-public class SimpleValue implements IValue
+import java.io.Serializable;
+
+public class SimpleValue implements IValue, Serializable
 {
+	private static final long serialVersionUID = 7661063626244223642L;
+	
 	public double value;
 	
 	public SimpleValue(double value)
